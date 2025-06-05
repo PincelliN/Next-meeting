eaa241c34ba1eed1b28a6224b6f8c3cfbde8cf14
Nicolò Pincelli <pincelli.n@gmail.com>
2025-06-05 11:19:01 +0200

    feat: add header

---
+++ src/components/header.tsx
@@ -0,0 +1,21 @@
+type HeadersProps = {
+  meetings: number;
+};
+
+export default function Headers({ meetings }: HeadersProps) {
+  let title: string;
+
+  if (meetings > 1) {
+    title = "Prossimi Appuntamenti";
+  } else if (meetings === 0) {
+    title = "Prossimo Appuntamento";
+  } else {
+    title = "Non hai appuntamenti";
+  }
+
+  return (
+    <>
+      <h1 className="d-flex justify-content-center pt-2">{title}</h1>
+    </>
+  );
+}
