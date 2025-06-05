dd69538ca8f3999c1eaa069ad49dc98ce493957b
Nicolò Pincelli <pincelli.n@gmail.com>
2025-06-05 12:14:51 +0200

    feat: layout meetings

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import "./App.css";
 import Headers from "./components/header";
-import GymGuy from "../public/tizio-palestra_naoqwo.jpg";
-import HeadphonesGirl from "../public/tizia-cuffie_xf2bae.jpg";
-import GlassesGirl from "../public/tipa-occhiali_svc9jp.jpg";
-import VeganGuy from "../public/tizio_awlq7r.jpg";
-import JacketGuy from "../public/tizio-giacca_jvzktv.jpg";
+import GymGuy from "./asset/tizio-palestra_naoqwo.jpg";
+import HeadphonesGirl from "./asset/tizia-cuffie_xf2bae.jpg";
+import GlassesGirl from "./asset/tipa-occhiali_svc9jp.jpg";
+import VeganGuy from "./asset/tizio_awlq7r.jpg";
+import JacketGuy from "./asset/tizio-giacca_jvzktv.jpg";
 import Meetings from "./components/meetings";
 import { Meeting } from "./types/meeting";
 
--- src/components/meetings.tsx
+++ src/components/meetings.tsx
@@ -7,18 +7,31 @@ type MeetingProps = {
 const Meetings: React.FC<MeetingProps> = ({ meetings }) => {
   return (
     <>
-      <div className="row row-cols-1 row-cols-md-2 g-4">
-        <div className="col">
-          <div className="card">
-            <img src="..." className="card-img-top" alt="..." />
-            <div className="card-body">
-              <h5 className="card-title">Card title</h5>
-              <p className="card-text">
-                This is a longer card with supporting text below as a natural
-                lead-in to additional content. This content is a little bit
-                longer.
-              </p>
-            </div>
+      <div className="container">
+        <div className="row g-5 m-auto">
+          <div className="col">
+            {meetings.map((el) => {
+              return (
+                <>
+                  <div className="card bg-secondary m-2" key={el.id}>
+                    <div className="card-body d-flex justify-content-between align-items-center ">
+                      <div className="d-flex justify-content-start align-items-center ">
+                        <img src={el.img} className="avatar" alt={el.name} />
+                        <section className="ms-2">
+                          <h5 className="h3 text-white">{el.name}</h5>
+                          <p className="text-white">
+                            <strong>{el.ideal}</strong>
+                          </p>
+                        </section>
+                      </div>
+                      <div>
+                        <button className="btn btn-danger">cancella</button>
+                      </div>
+                    </div>
+                  </div>
+                </>
+              );
+            })}
           </div>
         </div>
       </div>
